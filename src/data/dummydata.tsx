98115e6fe2fa59a4e12a5487f69c4f49456a58cc
unknown <ryan20bv@gmail.com>
2023-07-13 19:34:34 +0800

    adjust code for game index

--- src/data/dummydata.tsx
+++ src/data/dummydata.tsx
@@ -1,268 +1,174 @@
 import { ISaveGame, IGameTileData } from "./modelTypes";
 
-export const dummyData = [
+export const updatedGameData: IGameTileData[][] = [
+	[
+		{
+			tile_id: "1-1",
+			filled: true,
+			item: "X",
+		},
+		{
+			tile_id: "1-2",
+			filled: true,
+			item: "O",
+		},
+		{
+			tile_id: "1-3",
+			filled: false,
+			item: "",
+		},
+	],
+	[
+		{
+			tile_id: "2-1",
+			filled: false,
+			item: "",
+		},
+		{
+			tile_id: "2-2",
+			filled: false,
+			item: "",
+		},
+		{
+			tile_id: "2-3",
+			filled: false,
+			item: "",
+		},
+	],
+	[
+		{
+			tile_id: "3-1",
+			filled: false,
+			item: "",
+		},
+		{
+			tile_id: "3-2",
+			filled: false,
+			item: "",
+		},
+		{
+			tile_id: "3-3",
+			filled: false,
+			item: "",
+		},
+	],
+];
+
+export const gameData: IGameTileData[] = [
 	{
-		playerTurn: "1",
-		id: "g1",
-		player1: {
-			name: "Ray",
-			win: 1,
-		},
-		player2: {
-			name: "Jake",
-			win: 2,
-		},
-		draw: 2,
-		gameHistory: [
-			[
-				{
-					id: "1-1",
-					filled: true,
-					item: "X",
-				},
-				{
-					id: "1-2",
-					filled: true,
-					item: "O",
-				},
-				{
-					id: "1-3",
-					filled: false,
-					item: "",
-				},
-			],
-			[
-				{
-					id: "2-1",
-					filled: false,
-					item: "",
-				},
-				{
-					id: "2-2",
-					filled: false,
-					item: "",
-				},
-				{
-					id: "2-3",
-					filled: false,
-					item: "",
-				},
-			],
-			[
-				{
-					id: "3-1",
-					filled: false,
-					item: "",
-				},
-				{
-					id: "3-2",
-					filled: false,
-					item: "",
-				},
-				{
-					id: "3-3",
-					filled: false,
-					item: "",
-				},
-			],
-		],
+		tile_id: "1",
+		filled: true,
+		item: "X",
 	},
 	{
-		playerTurn: "2",
-		id: "g2",
-		player1: {
-			name: "Amon",
-			win: 3,
-		},
-		player2: {
-			name: "Jake",
-			win: 2,
-		},
-		draw: 0,
-		gameHistory: [
-			[
-				{
-					id: "1-1",
-					filled: true,
-					item: "X",
-				},
-				{
-					id: "1-2",
-					filled: true,
-					item: "O",
-				},
-				{
-					id: "1-3",
-					filled: false,
-					item: "",
-				},
-			],
-			[
-				{
-					id: "2-1",
-					filled: false,
-					item: "",
-				},
-				{
-					id: "2-2",
-					filled: false,
-					item: "",
-				},
-				{
-					id: "2-3",
-					filled: true,
-					item: "X",
-				},
-			],
-			[
-				{
-					id: "3-1",
-					filled: false,
-					item: "",
-				},
-				{
-					id: "3-2",
-					filled: false,
-					item: "",
-				},
-				{
-					id: "3-3",
-					filled: false,
-					item: "",
-				},
-			],
-		],
+		tile_id: "2",
+		filled: true,
+		item: "O",
 	},
 	{
-		playerTurn: "2",
-		id: "g3",
-		player1: {
-			name: "Lucky",
-			win: 2,
-		},
-		player2: {
-			name: "Miya",
-			win: 1,
-		},
-		draw: 1,
-		gameHistory: [
-			[
-				{
-					id: "1-1",
-					filled: true,
-					item: "X",
-				},
-				{
-					id: "1-2",
-					filled: true,
-					item: "O",
-				},
-				{
-					id: "1-3",
-					filled: false,
-					item: "",
-				},
-			],
-			[
-				{
-					id: "2-1",
-					filled: false,
-					item: "",
-				},
-				{
-					id: "2-2",
-					filled: true,
-					item: "X",
-				},
-				{
-					id: "2-3",
-					filled: false,
-					item: "",
-				},
-			],
-			[
-				{
-					id: "3-1",
-					filled: false,
-					item: "",
-				},
-				{
-					id: "3-2",
-					filled: false,
-					item: "",
-				},
-				{
-					id: "3-3",
-					filled: false,
-					item: "",
-				},
-			],
-		],
+		tile_id: "3",
+		filled: false,
+		item: "",
 	},
 	{
-		playerTurn: "1",
-		id: "g4",
-		player1: {
-			name: "Lucky",
-			win: 2,
-		},
-		player2: {
-			name: "Miya",
-			win: 1,
-		},
-		draw: 1,
-		gameHistory: [
-			[
-				{
-					id: "1-1",
-					filled: true,
-					item: "X",
-				},
-				{
-					id: "1-2",
-					filled: true,
-					item: "O",
-				},
-				{
-					id: "1-3",
-					filled: false,
-					item: "",
-				},
-			],
-			[
-				{
-					id: "2-1",
-					filled: true,
-					item: "X",
-				},
-				{
-					id: "2-2",
-					filled: false,
-					item: "",
-				},
-				{
-					id: "2-3",
-					filled: false,
-					item: "",
-				},
-			],
-			[
-				{
-					id: "3-1",
-					filled: true,
-					item: "O",
-				},
-				{
-					id: "3-2",
-					filled: false,
-					item: "",
-				},
-				{
-					id: "3-3",
-					filled: false,
-					item: "",
-				},
-			],
-		],
+		tile_id: "4",
+		filled: false,
+		item: "",
+	},
+	{
+		tile_id: "5",
+		filled: false,
+		item: "",
+	},
+	{
+		tile_id: "6",
+		filled: false,
+		item: "",
+	},
+	{
+		tile_id: "7",
+		filled: false,
+		item: "",
+	},
+	{
+		tile_id: "8",
+		filled: false,
+		item: "",
+	},
+	{
+		tile_id: "9",
+		filled: false,
+		item: "",
 	},
 ];
+
+export const startGameData: IGameTileData[] = [
+	{
+		tile_id: "1",
+		filled: true,
+		item: "TIK",
+	},
+	{
+		tile_id: "2",
+		filled: false,
+		item: "",
+	},
+	{
+		tile_id: "3",
+		filled: false,
+		item: "",
+	},
+	{
+		tile_id: "4",
+		filled: false,
+		item: "",
+	},
+	{
+		tile_id: "5",
+		filled: true,
+		item: "TAK",
+	},
+	{
+		tile_id: "6",
+		filled: false,
+		item: "",
+	},
+	{
+		tile_id: "7",
+		filled: false,
+		item: "",
+	},
+	{
+		tile_id: "8",
+		filled: false,
+		item: "",
+	},
+	{
+		tile_id: "9",
+		filled: true,
+		item: "TOE",
+	},
+];
+
+export const getAllSavedGames = () => {
+	return dummyData;
+};
+
+export const getSavedGamesById = (id: string) => {
+	const filteredGame = dummyData.find((indivGame: ISaveGame) => {
+		return indivGame._id === id;
+	});
+	return filteredGame;
+};
+
+export const getGameData = () => {
+	return gameData;
+};
+
+export const getStartGameData = () => {
+	return startGameData;
+};
+
+export const getUpdatedGameData = () => {
+	return updatedGameData;
+};
--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import type { InferGetStaticPropsType, GetStaticProps } from "next";
 import StartNewGame from "@/components/startNewGame/StartNewGame";
-import { getStartGameData } from "@/data/dummydata";
+
 import { IGameTileData } from "@/data/modelTypes";
 
 interface PropsType {
